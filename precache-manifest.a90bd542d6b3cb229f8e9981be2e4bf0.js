d3acc20e653e65a03c43c9133e1dc87410abc057
Guilherme <guilherme.ferreira28@outlook.com>
2019-11-22 20:18:10 +0100

    Updates

---
+++ precache-manifest.a90bd542d6b3cb229f8e9981be2e4bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b35c594a717634602be3afc0ecbdb3f",
+    "url": "/recipes-view/index.html"
+  },
+  {
+    "revision": "f6b12457d0ea18b49b45",
+    "url": "/recipes-view/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "3867e39ae68548aac57a",
+    "url": "/recipes-view/static/css/main.a5f2fffa.chunk.css"
+  },
+  {
+    "revision": "f6b12457d0ea18b49b45",
+    "url": "/recipes-view/static/js/2.cf064277.chunk.js"
+  },
+  {
+    "revision": "3867e39ae68548aac57a",
+    "url": "/recipes-view/static/js/main.924c9ef7.chunk.js"
+  },
+  {
+    "revision": "c1592d257af6c4c47562",
+    "url": "/recipes-view/static/js/runtime-main.15432ac7.js"
+  }
+]);
